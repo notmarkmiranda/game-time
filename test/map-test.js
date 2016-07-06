1acfb34c0f6304d8a8143731f6d2eda43c7d785f
Matt Pindell <pindell.m@gmail.com>
2016-07-06 13:50:22 -0600

    Create map-test

---
+++ test/map-test.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+
+const Map = require('../lib/map');
+
+describe('Map', function() {
+  context('with default attributes', function() {
+    var map = new Map({});
+
+    it('should have outer walls', function() {
+      // assert.equal(dingus.x, 0);
+    });
+
+  });
+});
