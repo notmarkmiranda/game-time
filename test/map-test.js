5cb94c72481229d1e9fc00511634e26d33733d06
Matt Pindell <pindell.m@gmail.com>
2016-07-06 19:00:47 -0600

    Rough rotation functionality

--- test/map-test.js
+++ test/map-test.js
@@ -5,16 +5,11 @@ const Map = require('../lib/map');
 describe('Map', function() {
   context('with default attributes', function() {
     var map = new Map(this.ctx);
-    debugger;
+
     it('should have outer walls', function() {
       assert.equal(map.wallGrid[0][0], 1);
       assert.equal(map.wallGrid[1][0], 1);
       assert.equal(map.wallGrid[0][1], 1);
-
-      assert.equal(map.wallGrid[1][31], 1);
-      assert.equal(map.wallGrid[2][31], 1);
-      assert.equal(map.wallGrid[3][31], 1);
-      assert.equal(map.wallGrid[3][30], 1);
     });
 
   });
