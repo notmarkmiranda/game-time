959b7ba2f65c5bd1db4e4879cc5603dc10cc34c7
Matt Pindell <pindell.m@gmail.com>
2016-07-14 11:28:58 -0600

    Add superGhost tests

---
+++ test/superGhost-test.js
@@ -0,0 +1,102 @@
+const assert = require('chai').assert;
+
+function Image(){};
+const SuperGhost = require('../lib/superGhost');
+
+const Map = require('../lib/map');
+const Player = require('../lib/player');
+
+const realMap = [
+	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+];
+
+describe('SuperGhost', function(){
+  context('with default attributes', function(){
+    let map = new Map({
+      ctx: this.ctx,
+      width: realMap[0].length,
+      height: realMap.length,
+      grid: realMap
+    });
+
+		const player = new Player(this.ctx);
+
+    let ghost = new SuperGhost({
+      ctx: this.ctx,
+      map: map
+    });
+
+    it('should have default attributes', function(){
+      assert.isAbove(ghost.x, 0)
+      assert.isAbove(ghost.y, 0)
+    });
+
+    it('should chase a player', function(){
+      ghost.x = 0;
+      ghost.y = 0;
+      let originX = ghost.x
+      let originY = ghost.y
+      ghost.chasePlayer(10, 10)
+      assert.notEqual(ghost.x, originX)
+      assert.notEqual(ghost.y, originY)
+    });
+
+    it('should be able to block check with type 1', function(){
+			assert.equal(ghost.blockCheck(0, 0, map).type, 1)
+    });
+
+    it('should be able to block check with type 0', function(){
+			assert.equal(ghost.blockCheck(10, 10, map).type, 0)
+    });
+
+		it('should be able to calculate the amounts to add to move towards position/player', function(){
+			ghost.x = 0;
+			ghost.y = 0;
+			let distances = ghost.distanceToPlayer(12, 12);
+			assert.isAbove(distances[0], 0);
+			assert.isAbove(distances[1], 0);
+		});
+
+		it('should move by x and y based on distance from player', function(){
+			ghost.x = 0;
+			ghost.y = 0;
+			ghost.chasePlayer(12, 12);
+			assert.isAbove(ghost.x, 0);
+			assert.isAbove(ghost.y, 0);
+		});
+
+		it('should be able to confirm a collision', function(){
+			player.pillCount = 1
+			assert.equal(player.pillCount, 1);
+			player.x = 0;
+			player.y = 0;
+			ghost.x = 0;
+			ghost.y = 0;
+			assert.notEqual(player.pillCount, 0)
+		})
+
+  });
+});
